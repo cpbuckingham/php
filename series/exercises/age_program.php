2fb55830d8453454e72da205d723d4f3e72825d8
Cam <cameron.p.buckingham@gmail.com>
2016-04-07 14:59:29 -0500

    age program

---
+++ series/exercises/age_program.php
@@ -0,0 +1,15 @@
+<?php
+$name = "cam";
+$age= 21;
+if(strtolower($name)==='cam'){
+  if ($age>=21){
+    echo "you're over 21";
+    if(1===1){
+      echo "yes 1 is equal to 1!";
+
+    }
+  }
+}else{
+  echo "you're not cam!";
+}
+?>
