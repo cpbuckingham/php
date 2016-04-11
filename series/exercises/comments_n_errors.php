ee5f183f85d471b5413217872c5494e1b612b976
Cam <cameron.p.buckingham@gmail.com>
2016-04-11 10:11:33 -0500

    error handling

--- series/exercises/comments_n_errors.php
+++ series/exercises/comments_n_errors.php
@@ -12,4 +12,8 @@ echo 'hello';
 echo $var = "alex";
 $var2 = "Billy";
 
+// e_all handles run time errors, if you make changes in .ini
+// restart servers before you run
+error_reporting(e_all);
+ini_set('error_reporting', e_strict);
 ?>
