dbbff32214ec1c6da32710b9e30830e25938178f
Cam <cameron.p.buckingham@gmail.com>
2016-04-13 10:22:47 -0500

    concatenation

---
+++ series/exercises/concatenation.php
@@ -0,0 +1,14 @@
+<?php
+
+$day = 'Thursday';
+$date = 31;
+$year = 2016;
+
+echo 'the date is thursday 31 2016 ';
+
+echo 'the date is <strong>' .$day. ' ' .$date. ' ' .$year. '</strong>';
+echo "the date is $day $date $year";
+
+//don't forget to use perids when you concat with variables in php
+
+ ?>
