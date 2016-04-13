7ced71a080d8a65fe9d72009eccf91cc835964a6
Cam <cameron.p.buckingham@gmail.com>
2016-04-13 10:23:00 -0500

    variables

---
+++ series/exercises/variables.php
@@ -0,0 +1,16 @@
+<?php
+
+//all variables start with dollar sign in php
+
+$name = "cam";
+$age = 28;
+$rank = "general";
+
+echo $name;
+echo ' is ';
+echo $age;
+echo ' years old, and he is a ';
+echo $rank;
+echo ' in his own army ';
+
+ ?>
