9e2c19f693c8fac03aacc0efb579ebbc97f7f9d1
Cam <cameron.p.buckingham@gmail.com>
2016-04-14 13:10:36 -0500

    assignment_ops

---
+++ series/exercises/assignment_ops.php
@@ -0,0 +1,28 @@
+<?php
+
+$number1 = 10;
+$number2 = 5;
+
+$result = $number1 + $number2;
+
+echo $result;
+
+$number3 = 10;
+$number3 +=2;
+
+/*
+
+/=
+*=
+-=
+
+*/
+
+echo $number3;
+
+$text = "hello";
+$text .= " world";
+
+echo $text; 
+
+?>
