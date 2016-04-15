4ec30adf4766f1c6e9b1bbe79a087c0c74b90e5a
Cam <cameron.p.buckingham@gmail.com>
2016-04-15 09:21:37 -0500

    triple_equals

---
+++ series/exercises/triple_equals.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = '1';
+$num = 1;
+
+if ($num1==$num){
+  echo 'equal';
+}else{
+  echo 'no equal';
+}
+echo '<br>';
+if ($num1===$num){
+  echo 'equal';
+}else{
+  echo 'no equal';
+}
+
+?>
