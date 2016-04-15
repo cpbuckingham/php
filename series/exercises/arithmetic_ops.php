fef8d446e0c5e387155e306864f961c6fa53fe56
Cam <cameron.p.buckingham@gmail.com>
2016-04-15 09:08:42 -0500

    arithmetic_ops

---
+++ series/exercises/arithmetic_ops.php
@@ -0,0 +1,19 @@
+<?php
+
+$sum = 10 + 20;
+
+echo abs($sum*20/10-400);
+echo '<br>';
+$num1 = 10;
+$num2 = 3;
+$num3 = 17;
+
+$result = $num1%$num2;
+
+echo $result;
+echo '<br>';
+echo $num3++;
+echo '<br>';
+echo $num3--;
+//operators are not working for this ....
+?>
