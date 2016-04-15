5eef4c41f8fdaa41935d7e874d9762ea5d6f1bc5
Cam <cameron.p.buckingham@gmail.com>
2016-04-15 09:17:16 -0500

    or and not for logical ops

--- series/exercises/logical_ops.php
+++ series/exercises/logical_ops.php
@@ -6,6 +6,8 @@ $upper = 1000;
 $lower = 500;
 
 if ($number1>=$lower && $number1<=$upper){
+  // or can be used with 2 pipes '||'
+  // not can be used with a bang '!'
 echo 'OK';
 }else{
   echo 'Number must be between' .$upper. ' and' .$lower;
