968165182e2d2368a07f7c1df3ba334ec3bb5f2b
Cam <cameron.p.buckingham@gmail.com>
2016-04-15 09:15:57 -0500

    logical_ops

---
+++ series/exercises/logical_ops.php
@@ -0,0 +1,15 @@
+<?php
+
+$number1 = 555;
+
+$upper = 1000;
+$lower = 500;
+
+if ($number1>=$lower && $number1<=$upper){
+echo 'OK';
+}else{
+  echo 'Number must be between' .$upper. ' and' .$lower;
+}
+
+
+?>
