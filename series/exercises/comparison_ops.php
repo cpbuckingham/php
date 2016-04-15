7851933efa6c32944bcbd3600bb2691390ea0507
Cam <cameron.p.buckingham@gmail.com>
2016-04-15 08:51:46 -0500

    comparison_ops

---
+++ series/exercises/comparison_ops.php
@@ -0,0 +1,24 @@
+<?php
+
+$number1 = 10;
+$number2 = 9;
+
+// if(1!=2){
+
+if($number1 <$number2){
+  // <> also is !=
+  echo 'inside';
+}else{
+  echo 'no';
+}
+
+$password = 'password';
+
+if($password == 'password'){
+  echo 'correct';
+}else{
+    echo 'incorrect';
+  }
+
+
+?>
