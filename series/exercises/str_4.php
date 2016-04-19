0472f57fe23cdfb126ea26e1e04d6877b4ab4f46
Cam <cameron.p.buckingham@gmail.com>
2016-04-19 12:25:51 -0500

    str_4

---
+++ series/exercises/str_4.php
@@ -0,0 +1,7 @@
+<?php
+$string = '     this is an example of a string';
+$string_trimmed = trim($string);
+// ltrim, rtrim, html_entities displays entity, addslashes, stripslashes
+echo $string_trimmed;
+
+?>
