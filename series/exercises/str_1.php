50dc9f7ba927bd370422da06269ddb9143cee0f6
Cam <cameron.p.buckingham@gmail.com>
2016-04-19 12:23:26 -0500

    str_1

---
+++ series/exercises/str_1.php
@@ -0,0 +1,8 @@
+<?php
+$string = 'this is an example of a string';
+$string_word_count = str_word_count($string, 1);
+//counts the words
+//$string_word_count = str_word_count($string, 2);
+//counts the characters
+print_r($string_word_count);
+?>
