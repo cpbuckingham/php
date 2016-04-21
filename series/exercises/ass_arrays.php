da371aa3aecbcb3ca80f25c517618cccea8a94d9
Cam <cameron.p.buckingham@gmail.com>
2016-04-21 13:14:22 -0500

    associative arrays

---
+++ series/exercises/ass_arrays.php
@@ -0,0 +1,10 @@
+<?php
+
+$people = array("cam" => 28, "jenna" =>30, "lindsey" =>28, "kenzi" =>32);
+print_r($people);
+
+//our key of cam is hold 28 value
+echo $people["cam"];
+//created an associative array
+
+?>
