f7ac8df6348c1aadde0b422dafd68085710f215d
Cam <cameron.p.buckingham@gmail.com>
2016-04-22 09:35:01 -0500

    multi_dim_arr

---
+++ series/exercises/multi_dim_arr.php
@@ -0,0 +1,10 @@
+<?php
+
+$cat = array('healthy'=>
+                array('salad', 'pasta', 'veggies'),
+            'unhealthy'=>
+                array('pizza'));
+
+//$food = array('Pasta'=>300, 'Pizza'=>1000, 'Salad'=>150, 'Veggies'=>50);
+echo $cat['healthy'][2];
+?>
