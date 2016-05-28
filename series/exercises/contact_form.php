10f342ef8135cb38ea08e64d63375d1094c921c9
Cam <cameron.p.buckingham@gmail.com>
2016-05-28 15:42:46 -0500

    contact form

---
+++ series/exercises/contact_form.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_POST['contact_name']) && isset($_POST['contact_email']) && )
+
+?>
+
+<form action="index.php" method="POST">
+  Name:<br><input type = "text" name="contact_name"><br><br>
+  Email:<br><input type = "text" name="contact_email"><br><br>
+  Message:<br>
+  <textarea name="contact_text" rows="6" cols="30"></textarea><br><br>
+  <input type="submit" value="Send">
+</form>
