6ca36fd716ba5e0aac0b3e338f3912975718322c
Cam <cameron.p.buckingham@gmail.com>
2016-05-31 08:41:02 -0500

    xml tags

---
+++ series/exercises/xml.php
@@ -0,0 +1,29 @@
+<?php
+
+//extensive markup language
+<producers>
+
+  <producer>
+    <name>cam</name>
+    <age>28</age>
+  </producer>
+
+  <producer>
+    <name>jenna</name>
+    <age>28</age>
+  </producer>
+
+  <producer>
+    <name>lindsey</name>
+    <age>28</age>
+  </producer>
+
+</producers>
+
+
+
+
+
+
+
+?>
