5b382a32d08a22ffc6a7e40bfe0e4bff26112323
Cam <cameron.p.buckingham@gmail.com>
2016-05-31 08:54:30 -0500

    interating XML

--- series/exercises/read_xml.php
+++ series/exercises/read_xml.php
@@ -3,7 +3,11 @@
 //extensible markup language
 
 $xml = simplexml_load_file('xml.xml');
-echo $xml->producer[0]->name.' '$xml->producer[0]->age;
+echo $xml->producer[0]->name.' is '.$xml->producer[0]->age;
+
+foreach ($xml->producer as $producer) {
+  echo $producer->name.' is '.$producer->age.'<br>';
+}
 
 //xml - everything is case sensitive
 ?>
